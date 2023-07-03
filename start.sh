4df743b2210741259f7d89d2b3b7ac5225a1eca5
petarvagner <petarvagner@msn.com>
2023-07-03 09:10:09 +0200

    add npm commands to start script

--- start.sh
+++ start.sh
@@ -4,4 +4,6 @@ cd ./docker/
 docker compose up -d && \
 docker compose run workspace composer install && \
 docker compose run workspace cp -n .env.example .env && \
-docker compose run workspace php artisan migrate
+docker compose run workspace php artisan migrate && \
+docker compose run workspace npm install && \
+docker compose run workspace npm run build

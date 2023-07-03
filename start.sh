d690fc1d57050ea8301f6823b7231c827a3f4621
petarvagner <petarvagner@msn.com>
2023-07-03 08:57:53 +0200

    fix failing cold start docker build

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 cd ./docker/
 
-docker compose up -d
-docker compose run workspace composer install
-docker compose run workspace cp -n .env.example .env
-docker compose run workspace art migrate
+docker compose up -d && \
+docker compose run workspace composer install && \
+docker compose run workspace cp -n .env.example .env && \
+docker compose run workspace php artisan migrate

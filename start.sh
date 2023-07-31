45a253debe01e9e46efd432b8aa34edbf866bca8
petarvagner <petarvagner@msn.com>
2023-07-31 10:38:54 +0200

    update start script

--- start.sh
+++ start.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
 cd ./docker/
 
-docker compose up -d && \
-docker compose run workspace composer install && \
-docker compose run workspace cp -n .env.example .env && \
-docker compose run workspace php artisan migrate && \
-docker compose run workspace npm install && \
+docker compose up -d
+
+# wait for the containers to come up
+until docker compose up -d; do
+    echo "Waiting for containers..."
+    sleep 0.5
+done
+
+docker compose run workspace composer install
+docker compose run workspace cp -n .env.example .env
+docker compose run workspace php artisan migrate
+docker compose run workspace npm install
 docker compose run workspace npm run build
